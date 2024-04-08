f13518a7957d602dec88f133473e3c828435eb27
Beatriz Tavares <126013004+BeatrizTavaresL@users.noreply.github.com>
2024-04-08 18:39:08 -0300

    Add files via upload

---
+++ Desafio4_Calculadora_IMC.py
@@ -0,0 +1,26 @@
+''' Calculo de IMC - Indice de Massa Corporal 
+Menor que 18,5 - MAGREZA
+Entre 18,25 e 24,9 - NORMAL
+Entre 25,0 e 29,9 - SOBREPESO
+Entre 30,0 e 39,9 - OBESIDADE
+Maior que 40,0 - OBESIDADE GRAVE 
+'''
+
+altura = float(input('Qual é a sua altura em cm? '))
+peso = float(input('Qual é o seu peso em Kg? '))
+
+IMC = peso / (altura/100)**2
+
+if IMC < 18.5:
+    print('MAGREZA!')
+elif IMC >= 18.5 and IMC < 24.9:
+    print('NORMAL!')
+elif IMC >= 25.0 and IMC < 29.9:
+    print('SOBREPESO!')
+elif IMC >= 30.0 and IMC < 39.9:
+    print('OBESIDADE!')
+else:
+    print('OBESIDADE GRAAVE!')
+
+
+
